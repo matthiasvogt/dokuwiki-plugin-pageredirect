558fe4559ea7a258063e43760667ac72ec25d880
Matthias Vogt <matthias.vogt@check24.de>
2013-05-29 15:44:01 +0200

    added german language

---
+++ lang/de/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * German language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Matthias Vogt <exdo@gmx.de>
+ */
+
+$lang['redirect_to']     = "Diese Seite wurde verschoben, der neue Namensraum ist %s.";
+$lang['redirected_from'] = "Sie wurden weitergeleitet von %s.";
---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * German language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Matthias Vogt <exdo@gmx.de>
+ */
+
+$lang['show_note'] = 'Umleitungshinweis auf der Zielseite anzeigen?';
